c74f1d29e9e52c06ccf9b6c3d365b6c0eb021af5
Mukesh-h-Programmer <midasmukesh6@gmail.com>
2021-07-05 15:12:03 +0530

    Intial commit

---
+++ Guessing__Game.py
@@ -0,0 +1,14 @@
+secret__number = 9
+guess__count = 0
+guess__limit = 3
+print('You have only thrice chance ! ''\n''Lets start...')
+while guess__count < guess__limit:
+    guess = int(input('Guess : '))
+    guess__count += 1
+    if guess == secret__number:
+        print('You won :)')
+        break
+    elif guess__count == guess__limit:
+        print('Sorry, you failed :(''\n''Never mind play again 👍')
+    else:
+        print('Sorry, you failed :( ')
